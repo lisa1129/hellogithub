b9e995fca4e3c7107b16896ea2ae4ef412973674
Sohee Moon <shm8485@e-mirim.hs.kr>
2017-03-28 21:04:37 +0900

    #1 Button add

--- app/src/main/java/kr/hs/e_mirim/lisa1129/helloworld/MainActivity.java
+++ app/src/main/java/kr/hs/e_mirim/lisa1129/helloworld/MainActivity.java
@@ -1,16 +1,27 @@
 package kr.hs.e_mirim.lisa1129.helloworld;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnHello=(Button)findViewById(R.id.btnHello);
+        btnHello.setOnClickListener(this);
+       }
+
+    @Override
+    public void onClick(View v) {
+
+        Toast.makeText(this, "Hello", Toast.LENGTH_LONG).show();
+
     }
-
-
-
 }
